1536d1261faf5c16415c0f118e606e4fd5ee94a9
acetil <acetildev@gmail.com>
2021-12-01 21:27:19 +1100

    Added day1 solution

---
+++ 2021/day1.py
@@ -0,0 +1,7 @@
+f = open("day1.txt")
+
+vals = [int(i) for i in f.readlines()]
+
+print(f"Num increased: {sum(1 for i in range(1, len(vals)) if vals[i] > vals[i - 1])}")
+
+print(f"Sliding num increased: {sum(1 for i in range(1, len(vals) - 2) if sum(vals[i:i + 3]) > sum(vals[i - 1:i + 2]))}")
