51b9f7aa583bfed9a38e61eb8ace8442691e6ab9
acetil <acetildev@gmail.com>
2021-12-02 19:33:05 +1100

    Added solution for day 2

---
+++ 2021/day02.py
@@ -0,0 +1,25 @@
+f = open("day02.txt")
+
+totalVec1 = (0, 0)
+totalVec2 = (0, 0)
+aim = 0
+
+for i in f.readlines():
+    l = i.split(" ")
+    vec = (0, 0)
+
+    if l[0] == "forward":
+        vec = [int(l[1]), 0]
+        totalVec2 = (totalVec2[0] + int(l[1]), totalVec2[1] + int(l[1]) * aim)
+    elif l[0] == "down":
+        vec = [0, int(l[1])]
+        aim += int(l[1])
+    elif l[0] == "up":
+        vec = [0, -int(l[1])]
+        aim -= int(l[1])
+
+    totalVec1 = (totalVec1[0] + vec[0], totalVec1[1] + vec[1])
+
+
+print(f"Multiplied position part 1: {totalVec1[0] * totalVec1[1]}")
+print(f"Multiplied position part 2: {totalVec2[0] * totalVec2[1]}")
