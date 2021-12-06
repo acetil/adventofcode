639263aef42e460651b1086f9ab5355d4d0961ab
acetil <acetildev@gmail.com>
2021-12-06 18:58:19 +1100

    Added solution for day 6

---
+++ 2021/day06.py
@@ -0,0 +1,22 @@
+def updateFish (buckets):
+    doubling = buckets[0]
+    buckets = [i for i in buckets[1:]]
+    buckets.append(doubling)
+    buckets[6] += doubling
+
+    return buckets
+
+f = open("day06.txt")
+
+l = [int(i) for i in f.readline().split(",")]
+
+
+buckets = [l.count(0), l.count(1), l.count(2), l.count(3), l.count(4), l.count(5), l.count(6), l.count(7), l.count(8)]
+
+for i in range(0, 256):
+    buckets = updateFish(buckets)
+
+    if i == 79:
+        print(f"Num lanternfish after 80 days: {sum(buckets)}")
+
+print(f"Num lanternfish after 256 days: {sum(buckets)}")
